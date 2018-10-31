809fe9cb606848e9c6ef9b4c0322e9a9c0bf3fa1
Hend Alkittawi <halkittawi@gmail.com>
2018-10-31 14:16:57 -0500

    initial commit

---
+++ list.js
@@ -0,0 +1,28 @@
+[
+    {
+        name: "PlayerA",
+        team: "teamA",
+        sport: "soccer",
+        url: "https://google.com",
+        image: "https://www.shape.com/sites/shape.com/files/styles/slide/public/gettyimages-902361588.jpg",
+        thumbnailImage: "https://img.fifa.com/image/upload/t_l3/nbrc75jgkjtfkuigolq5.jpg"
+    },
+
+    {
+        name: "PlayerB",
+        team: "teamB",
+        sport: "basketball",
+        url: "https://google.com",
+        image: "https://www.shape.com/sites/shape.com/files/styles/slide/public/gettyimages-902361588.jpg",
+        thumbnailImage: "https://www.shape.com/sites/shape.com/files/styles/slide/public/olivier.jpg"
+    },
+
+    {
+        name:"PlayerC",
+        team: "teamC",
+        sport: "football",
+        url: "https://google.com",
+        image: "https://www.shape.com/sites/shape.com/files/styles/slide/public/gettyimages-902361588.jpg",
+        thumbnailImage: "https://www.shape.com/sites/shape.com/files/styles/slide/public/gettyimages-902361588.jpg"
+    }
+]
